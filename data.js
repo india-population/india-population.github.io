7991003b1649f3a2be793add75b83f118f24fe22
sivagnanam <gnanamsiva@outlook.com>
2015-11-17 15:26:18 +0530

    data.js alignment

--- data.js
+++ data.js
@@ -1,79 +1,92 @@
-Lines = [['india', {
-			type: "spline",
-                        color: "#FF0000",
-			dataPoints: [
-			  { x: new Date('1960'), y: 449661874 },
-			  { x: new Date('1961'), y: 458691457 },
-			  { x: new Date('1962'), y: 468054145 },
-			  { x: new Date('1963'), y: 477729958 },
-			  { x: new Date('1964'), y: 487690114 },
-			  { x: new Date('1965'), y: 497920270 },
-			  { x: new Date('1966'), y: 508402908 },
-			  { x: new Date('1967'), y: 519162069 },
-			  { x: new Date('1968'), y: 530274729 },
-			  { x: new Date('1969'), y: 541844848 },
-				{ x: new Date('1970'), y: 553943226 },
-				{ x: new Date('1971'), y: 566605402 },
-				{ x: new Date('1972'), y: 579800632 },
-				{ x: new Date('1973'), y: 593451889 },
-				{ x: new Date('1974'), y: 607446519 },
-				{ x: new Date('1975'), y: 621703641 },
-				{ x: new Date('1976'), y: 636182810 },
-				{ x: new Date('1977'), y: 650907559 },
-				{ x: new Date('1978'), y: 665936435 },
-				{ x: new Date('1979'), y: 681358553 },
-				{ x: new Date('1980'), y: 697229745 },
-				{ x: new Date('1981'), y: 713561406 },
-				{ x: new Date('1982'), y: 730303461 },
-				{ x: new Date('1983'), y: 747374856 },
-				{ x: new Date('1984'), y: 764664278 },
-				{ x: new Date('1985'), y: 782085127 },
-				{ x: new Date('1986'), y: 799607235 },
-				{ x: new Date('1987'), y: 817232241 },
-				{ x: new Date('1988'), y: 834944397 },
-				{ x: new Date('1989'), y: 852736160 },
-				{ x: new Date('1990'), y: 870601776 },
-				{ x: new Date('1991'), y: 888513869 },
-				{ x: new Date('1992'), y: 906461358 },
-				{ x: new Date('1993'), y: 924475633 },
-				{ x: new Date('1994'), y: 942604211 },
-				{ x: new Date('1995'), y: 960874982 },
-				{ x: new Date('1996'), y: 979290432 },
-				{ x: new Date('1997'), y: 997817250 },
-				{ x: new Date('1998'), y: 1016402907},
-				{ x: new Date('1999'), y: 1034976626},
-				{ x: new Date('2000'), y: 1053481072},
-			  { x: new Date('2001'), y: 1071888190 },
-			  { x: new Date('2002'), y: 1090189358 },
-			  { x: new Date('2003'), y: 1108369577 },
-			  { x: new Date('2004'), y: 1126419321 },
-			  { x: new Date('2005'), y: 1144326293 },
-			  { x: new Date('2006'), y: 1162088305 },
-			  { x: new Date('2007'), y: 1179685631 },
-			  { x: new Date('2008'), y: 1197070109 },
-			  { x: new Date('2009'), y: 1214182182 },
-			  { x: new Date('2010'), y: 1230984504 },
-			  { x: new Date('2011'), y: 1247446011 },
-			  { x: new Date('2012'), y: 1263589639 },
-			  { x: new Date('2013'), y: 1279498874 },
-			  { x: new Date('2014'), y: 1295291543 }
-			]
-		}], ['tamilnadu',{
-			type: "spline",
-                        color: "#00ff00",
-			dataPoints: [
-				{ x: new Date('2011'), y: 72138958 }
-			]
-		}], ['kerala', {
-			type: "spline",
-                        color: "#66ff99",
-			dataPoints: [
-				{ x: new Date('2011'), y: 33387677 }
-			]
-		}], ['karnadaka', {
-			type: "spline",
-                        color: "#ff33cc",
-			dataPoints: [
-				{ x: new Date('2011'), y: 61130704 }
-			]
-		}]];
+Lines = [
+  ['india',
+    {
+      type: "spline",
+      color: "#FF0000",
+      dataPoints: [
+        { x: new Date('1960'), y: 449661874 },
+        { x: new Date('1961'), y: 458691457 },
+	{ x: new Date('1962'), y: 468054145 },
+	{ x: new Date('1963'), y: 477729958 },
+	{ x: new Date('1964'), y: 487690114 },
+	{ x: new Date('1965'), y: 497920270 },
+	{ x: new Date('1966'), y: 508402908 },
+	{ x: new Date('1967'), y: 519162069 },
+	{ x: new Date('1968'), y: 530274729 },
+	{ x: new Date('1969'), y: 541844848 },
+	{ x: new Date('1970'), y: 553943226 },
+	{ x: new Date('1971'), y: 566605402 },
+	{ x: new Date('1972'), y: 579800632 },
+	{ x: new Date('1973'), y: 593451889 },
+	{ x: new Date('1974'), y: 607446519 },
+	{ x: new Date('1975'), y: 621703641 },
+	{ x: new Date('1976'), y: 636182810 },
+	{ x: new Date('1977'), y: 650907559 },
+	{ x: new Date('1978'), y: 665936435 },
+	{ x: new Date('1979'), y: 681358553 },
+	{ x: new Date('1980'), y: 697229745 },
+	{ x: new Date('1981'), y: 713561406 },
+	{ x: new Date('1982'), y: 730303461 },
+	{ x: new Date('1983'), y: 747374856 },
+	{ x: new Date('1984'), y: 764664278 },
+	{ x: new Date('1985'), y: 782085127 },
+	{ x: new Date('1986'), y: 799607235 },
+	{ x: new Date('1987'), y: 817232241 },
+	{ x: new Date('1988'), y: 834944397 },
+	{ x: new Date('1989'), y: 852736160 },
+	{ x: new Date('1990'), y: 870601776 },
+	{ x: new Date('1991'), y: 888513869 },
+	{ x: new Date('1992'), y: 906461358 },
+	{ x: new Date('1993'), y: 924475633 },
+	{ x: new Date('1994'), y: 942604211 },
+	{ x: new Date('1995'), y: 960874982 },
+	{ x: new Date('1996'), y: 979290432 },
+	{ x: new Date('1997'), y: 997817250 },
+	{ x: new Date('1998'), y: 1016402907},
+	{ x: new Date('1999'), y: 1034976626},
+	{ x: new Date('2000'), y: 1053481072},
+	{ x: new Date('2001'), y: 1071888190 },
+	{ x: new Date('2002'), y: 1090189358 },
+	{ x: new Date('2003'), y: 1108369577 },
+	{ x: new Date('2004'), y: 1126419321 },
+	{ x: new Date('2005'), y: 1144326293 },
+	{ x: new Date('2006'), y: 1162088305 },
+	{ x: new Date('2007'), y: 1179685631 },
+	{ x: new Date('2008'), y: 1197070109 },
+	{ x: new Date('2009'), y: 1214182182 },
+	{ x: new Date('2010'), y: 1230984504 },
+	{ x: new Date('2011'), y: 1247446011 },
+	{ x: new Date('2012'), y: 1263589639 },
+	{ x: new Date('2013'), y: 1279498874 },
+	{ x: new Date('2014'), y: 1295291543 }
+      ] 
+    }
+  ],
+  ['tamilnadu',
+    {
+      type: "spline",
+      color: "#00ff00",
+      dataPoints: [
+        { x: new Date('2011'), y: 72138958 }
+      ]
+    }
+  ],
+  ['kerala',
+    {
+      type: "spline",
+      color: "#66ff99",
+      dataPoints: [
+        { x: new Date('2011'), y: 33387677 }
+      ]
+    }
+  ],
+  ['karnadaka',
+    {
+      type: "spline",
+      color: "#ff33cc",
+      dataPoints: [
+        { x: new Date('2011'), y: 61130704 }
+      ]
+    }
+  ]
+];
